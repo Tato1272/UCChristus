887abb3c80c6e875c8b8519ebc5042c775336428
Tato1272 <81763862+Tato1272@users.noreply.github.com>
2023-10-04 01:26:53 -0300

    Agregar Paciente
    Se agrego la funcion a api_datos, al html y al javascript para poder agregar pacientes

---
+++ Api_Datos.py
@@ -0,0 +1,87 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import sqlite3
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Configuramos el CORS para permitir solicitudes del html
+origins = [
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Creamos una Función que permita conectarnos a la base de datos
+def connect_to_db():
+    return sqlite3.connect('ucchristus.db')
+
+class Paciente(BaseModel):
+    nombre: str
+    apellido: str
+    correo: str
+    genero: str
+    plan: str
+    telefono: str
+
+def connect_to_db():
+    return sqlite3.connect('ucchristus.db')
+# Definimos la ruta que se utilizara para obtener los datos por id o email
+# ejemplo ruta http://127.0.0.1:8000/obtener_datos/sfeasley2@gnu.org
+@app.get("/obtener_datos/{filtro}")
+def obtener_datos(filtro: str):
+    # Nos conectamos a la base de datos con la funcion creada anteriormente
+    conexion = connect_to_db()
+    cursor = conexion.cursor()
+
+    # Generamos la consulta para obtener los datos filtrados por id
+    cursor.execute(f"SELECT * FROM pacientes WHERE id = ? or email = ?", (filtro,filtro))
+    resultados = cursor.fetchall()
+
+    # Cerramos la conexión a la base de datos
+    conexion.close()
+
+    # Retornamos el paciente
+    return {"paciente": resultados}
+
+# Definimos la ruta que se utilizara para insertar datos a la base de datos
+@app.post("/insertar_paciente/")
+def insertar_paciente(paciente: Paciente):
+    # Nos conectamos a la base de datos con la funcion creada anteriormente
+    conexion = connect_to_db()
+    cursor = conexion.cursor()
+    try:
+        # Generamos la consulta para obtener los id de los pacientes
+        cursor.execute("SELECT id FROM pacientes")
+        todos_los_ids = cursor.fetchall()
+
+        # Si encontramos id's los convertimos a numero, extraemos el mayor y sumamos 1
+        if todos_los_ids:
+            ids_numericos = [int(id[0]) for id in todos_los_ids]
+            max_id = max(ids_numericos)
+            nuevo_id = str(max_id + 1)
+        # si no encontramos id's definimos 1 como id
+        else:
+            nuevo_id = "1"
+        # Ejecumanos la consulta para la inyeccion de los datos 
+        cursor.execute(
+            "INSERT INTO pacientes(id, first_name, last_name, email, gender, [Plan de Salud], phone) VALUES (?, ?, ?, ?, ?, ?, ?)",
+            (nuevo_id, paciente.nombre, paciente.apellido, paciente.correo, paciente.genero, paciente.plan, paciente.telefono)
+        )
+        
+        # Cargamos los cambios y cerramos la conexion a la base de datos 
+        conexion.commit()
+        conexion.close()
+
+        # Retornamos un mensaje con el id del paciente insertado
+        return {"mensaje": f"Paciente insertado con éxito, ID: {nuevo_id}"}
+    except Exception as e:
+        conexion.rollback()
+        # Controlamos el error 
+        return HTTPException(status_code=500, detail=f"Error al insertar el paciente: {str(e)}")
--- home.js
+++ home.js
@@ -1,8 +1,9 @@
 // Traemos la tabla vacia en donde se mostraran los datos
-const apiDataElement = document.getElementById('api-data');
+const tabla = document.getElementById('api-data');
 
 // Definimos la url de la api a utilizar
 const apiUrl = ('http://127.0.0.1:8000/obtener_datos/')
+const apiUrl2 = 'http://127.0.0.1:8000/insertar_paciente/';
 
 // Creamos la funcion que nos busque al paciente
 function buscar() {
@@ -26,6 +27,7 @@ function buscar() {
                 // Mostramos una alerta en caso de que no se hayan encontrado pacientes
                 alert('Paciente no encontrado');
             } else {
+                document.getElementById("tabla").style.display = "block";
                 // Creamos las filas de la tabla con los datos obtenidos
                 const rows = data.paciente.map(paciente => {
                     return `<tr>
@@ -40,11 +42,48 @@ function buscar() {
                 });
 
                 // Agregamos las filas a la tabla
-                apiDataElement.innerHTML = rows.join('');
+                tabla.innerHTML = rows.join('');
             }
         })
         .catch(error => {
             // Manejamos los errores en caso de que se obtengan
             console.error('Hubo un error al obtener los datos:', error);
         });
+}
+
+// Creamos la funcion para la insercion de los datos 
+function insertar() {
+
+    // Definimos de donde se extraeran los datos 
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var correo = document.getElementById('correo').value;
+    var genero = document.getElementById('genero').value;
+    var plan = document.getElementById('plan').value;
+    var telefono = document.getElementById('telefono').value;
+
+    // Realizamos la solicitud POST a la api 
+    fetch(apiUrl2,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            nombre: nombre,
+            apellido: apellido,
+            correo: correo,
+            genero: genero,
+            plan: plan,
+            telefono: telefono
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Informamos la creacion del paciente 
+        alert(data.mensaje);
+    })
+    // Controlamos el error 
+    .catch(error => {
+        console.error('Hubo un error al insertar el paciente:', error);
+    });
 }
